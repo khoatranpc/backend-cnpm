a317344633fe03aba4830e1d05982cfebaac790e
Trần Đăng Khoa <khoatranpc603@gmail.com>
2022-05-26 10:18:28 +0700

    update

--- server/modules/Tours/index.js
+++ server/modules/Tours/index.js
@@ -233,22 +233,20 @@ const Tour = {
             }
             else {
                 const findDetailTour = await detailBookTourModel.findOne({ id_tour: id_tour });
-                let boolen = detailGuideTour.id_detail_tour.map(async (item, index) => {
+                let couter = detailGuideTour.id_detail_tour.filter(async (item, index) => {
                     const detail = await detailBookTourModel.findById(item.id_detail_Tour);
-                    console.log(detail);
-                    // cần test thêm
-                    console.log(detail.date_end_tour < findDetailTour.date_begin_tour);
-                    if (detail.date_end_tour < findDetailTour.date_begin_tour) {
-                        const addTour = await detailGuideTourModel.findOne({ id_user: id_guide });
-                        await addTour.updateOne({ $push: { id_detail_tour: id_tour } });
-                        res.status(200).send({
-                            message: "Thêm thành công"
-                        })
-                    } else {
-                        return false;
-                    }
+                    return detail.date_end_tour < findDetailTour.date_begin_tour ? item : '';
                 })
-                if (boolen === false) console.log('Người dẫn tour bị trùng lịch!');
+                if (couter.length != 0) {
+                    const addTour = await detailGuideTourModel.findOne({ id_user: id_guide });
+                    await addTour.updateOne({ $push: { id_detail_tour: id_tour } });
+                    res.status(200).send({
+                        message: "Thêm thành công"
+                    })
+                }
+                else {
+                    throw new Error("Không thêm được! Do người dẫn tour trùng lịch");
+                }
             }
         } catch (error) {
             res.status(500).send({
